80690be50e924a480e6be73b6f94f6cc1a0b0d6a
Cristian Sanchez <sancas19@gmail.com>
2015-09-17 16:00:31 -0600

    Agregadas nuevas guias
    Nuevas guias agregadas al repositorio

---
+++ Investigacion Complementaria - G6/Arbol Binario/Arbol Binario/Form1.Designer.cs
@@ -0,0 +1,287 @@
+﻿namespace Arbol_Binario
+{
+    partial class frmPrincipal
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.btnAddNode = new System.Windows.Forms.Button();
+            this.btnDelNode = new System.Windows.Forms.Button();
+            this.groupBox1 = new System.Windows.Forms.GroupBox();
+            this.lblSumas = new System.Windows.Forms.Label();
+            this.lblSumatoria = new System.Windows.Forms.Label();
+            this.rbtnMultiplosCinco = new System.Windows.Forms.RadioButton();
+            this.rbtnMultiplosTres = new System.Windows.Forms.RadioButton();
+            this.rbtnMultiplosDos = new System.Windows.Forms.RadioButton();
+            this.rbtnTotal = new System.Windows.Forms.RadioButton();
+            this.lblAltura = new System.Windows.Forms.Label();
+            this.btnPreOrden = new System.Windows.Forms.Button();
+            this.lblRecorridos = new System.Windows.Forms.Label();
+            this.btnPostOrden = new System.Windows.Forms.Button();
+            this.btnInOrden = new System.Windows.Forms.Button();
+            this.txtNumero = new System.Windows.Forms.TextBox();
+            this.lblValor = new System.Windows.Forms.Label();
+            this.groupBox1.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // btnAddNode
+            // 
+            this.btnAddNode.Enabled = false;
+            this.btnAddNode.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnAddNode.Location = new System.Drawing.Point(12, 77);
+            this.btnAddNode.Name = "btnAddNode";
+            this.btnAddNode.Size = new System.Drawing.Size(161, 36);
+            this.btnAddNode.TabIndex = 0;
+            this.btnAddNode.Text = "Agregar Nodo";
+            this.btnAddNode.UseVisualStyleBackColor = true;
+            this.btnAddNode.Click += new System.EventHandler(this.btnAddNode_Click);
+            // 
+            // btnDelNode
+            // 
+            this.btnDelNode.Enabled = false;
+            this.btnDelNode.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnDelNode.Location = new System.Drawing.Point(12, 119);
+            this.btnDelNode.Name = "btnDelNode";
+            this.btnDelNode.Size = new System.Drawing.Size(161, 36);
+            this.btnDelNode.TabIndex = 1;
+            this.btnDelNode.Text = "Eliminar Nodo";
+            this.btnDelNode.UseVisualStyleBackColor = true;
+            this.btnDelNode.Click += new System.EventHandler(this.btnDelNode_Click);
+            // 
+            // groupBox1
+            // 
+            this.groupBox1.Controls.Add(this.lblSumas);
+            this.groupBox1.Controls.Add(this.lblSumatoria);
+            this.groupBox1.Controls.Add(this.rbtnMultiplosCinco);
+            this.groupBox1.Controls.Add(this.rbtnMultiplosTres);
+            this.groupBox1.Controls.Add(this.rbtnMultiplosDos);
+            this.groupBox1.Controls.Add(this.rbtnTotal);
+            this.groupBox1.Controls.Add(this.lblAltura);
+            this.groupBox1.Controls.Add(this.btnPreOrden);
+            this.groupBox1.Controls.Add(this.lblRecorridos);
+            this.groupBox1.Controls.Add(this.btnPostOrden);
+            this.groupBox1.Controls.Add(this.btnInOrden);
+            this.groupBox1.Controls.Add(this.txtNumero);
+            this.groupBox1.Controls.Add(this.lblValor);
+            this.groupBox1.Controls.Add(this.btnDelNode);
+            this.groupBox1.Controls.Add(this.btnAddNode);
+            this.groupBox1.Dock = System.Windows.Forms.DockStyle.Left;
+            this.groupBox1.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.groupBox1.Location = new System.Drawing.Point(0, 0);
+            this.groupBox1.Name = "groupBox1";
+            this.groupBox1.Size = new System.Drawing.Size(186, 527);
+            this.groupBox1.TabIndex = 3;
+            this.groupBox1.TabStop = false;
+            this.groupBox1.Text = "Opciones";
+            // 
+            // lblSumas
+            // 
+            this.lblSumas.AutoSize = true;
+            this.lblSumas.Location = new System.Drawing.Point(12, 304);
+            this.lblSumas.Name = "lblSumas";
+            this.lblSumas.Size = new System.Drawing.Size(63, 20);
+            this.lblSumas.TabIndex = 4;
+            this.lblSumas.Text = "Sumas:";
+            // 
+            // lblSumatoria
+            // 
+            this.lblSumatoria.AutoSize = true;
+            this.lblSumatoria.Dock = System.Windows.Forms.DockStyle.Bottom;
+            this.lblSumatoria.Location = new System.Drawing.Point(3, 484);
+            this.lblSumatoria.Name = "lblSumatoria";
+            this.lblSumatoria.Size = new System.Drawing.Size(86, 20);
+            this.lblSumatoria.TabIndex = 14;
+            this.lblSumatoria.Text = "Sumatoria:";
+            this.lblSumatoria.Visible = false;
+            // 
+            // rbtnMultiplosCinco
+            // 
+            this.rbtnMultiplosCinco.AutoSize = true;
+            this.rbtnMultiplosCinco.Enabled = false;
+            this.rbtnMultiplosCinco.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.rbtnMultiplosCinco.Location = new System.Drawing.Point(12, 417);
+            this.rbtnMultiplosCinco.Name = "rbtnMultiplosCinco";
+            this.rbtnMultiplosCinco.Size = new System.Drawing.Size(102, 24);
+            this.rbtnMultiplosCinco.TabIndex = 13;
+            this.rbtnMultiplosCinco.TabStop = true;
+            this.rbtnMultiplosCinco.Text = "Multiplos 5";
+            this.rbtnMultiplosCinco.UseVisualStyleBackColor = true;
+            this.rbtnMultiplosCinco.CheckedChanged += new System.EventHandler(this.rbtnMultiplosCinco_CheckedChanged);
+            // 
+            // rbtnMultiplosTres
+            // 
+            this.rbtnMultiplosTres.AutoSize = true;
+            this.rbtnMultiplosTres.Enabled = false;
+            this.rbtnMultiplosTres.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.rbtnMultiplosTres.Location = new System.Drawing.Point(12, 387);
+            this.rbtnMultiplosTres.Name = "rbtnMultiplosTres";
+            this.rbtnMultiplosTres.Size = new System.Drawing.Size(102, 24);
+            this.rbtnMultiplosTres.TabIndex = 12;
+            this.rbtnMultiplosTres.TabStop = true;
+            this.rbtnMultiplosTres.Text = "Multiplos 3";
+            this.rbtnMultiplosTres.UseVisualStyleBackColor = true;
+            this.rbtnMultiplosTres.CheckedChanged += new System.EventHandler(this.rbtnMultiplosTres_CheckedChanged);
+            // 
+            // rbtnMultiplosDos
+            // 
+            this.rbtnMultiplosDos.AutoSize = true;
+            this.rbtnMultiplosDos.Enabled = false;
+            this.rbtnMultiplosDos.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.rbtnMultiplosDos.Location = new System.Drawing.Point(12, 357);
+            this.rbtnMultiplosDos.Name = "rbtnMultiplosDos";
+            this.rbtnMultiplosDos.Size = new System.Drawing.Size(102, 24);
+            this.rbtnMultiplosDos.TabIndex = 11;
+            this.rbtnMultiplosDos.TabStop = true;
+            this.rbtnMultiplosDos.Text = "Multiplos 2";
+            this.rbtnMultiplosDos.UseVisualStyleBackColor = true;
+            this.rbtnMultiplosDos.CheckedChanged += new System.EventHandler(this.rbtnMultiplosDos_CheckedChanged);
+            // 
+            // rbtnTotal
+            // 
+            this.rbtnTotal.AutoSize = true;
+            this.rbtnTotal.Enabled = false;
+            this.rbtnTotal.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.rbtnTotal.Location = new System.Drawing.Point(12, 327);
+            this.rbtnTotal.Name = "rbtnTotal";
+            this.rbtnTotal.Size = new System.Drawing.Size(62, 24);
+            this.rbtnTotal.TabIndex = 10;
+            this.rbtnTotal.TabStop = true;
+            this.rbtnTotal.Text = "Total";
+            this.rbtnTotal.UseVisualStyleBackColor = true;
+            this.rbtnTotal.CheckedChanged += new System.EventHandler(this.rbtnTotal_CheckedChanged);
+            // 
+            // lblAltura
+            // 
+            this.lblAltura.AutoSize = true;
+            this.lblAltura.Dock = System.Windows.Forms.DockStyle.Bottom;
+            this.lblAltura.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lblAltura.Location = new System.Drawing.Point(3, 504);
+            this.lblAltura.Name = "lblAltura";
+            this.lblAltura.Size = new System.Drawing.Size(55, 20);
+            this.lblAltura.TabIndex = 9;
+            this.lblAltura.Text = "Altura:";
+            this.lblAltura.Visible = false;
+            // 
+            // btnPreOrden
+            // 
+            this.btnPreOrden.Enabled = false;
+            this.btnPreOrden.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnPreOrden.Location = new System.Drawing.Point(12, 223);
+            this.btnPreOrden.Name = "btnPreOrden";
+            this.btnPreOrden.Size = new System.Drawing.Size(161, 36);
+            this.btnPreOrden.TabIndex = 8;
+            this.btnPreOrden.Text = "Pre-Orden";
+            this.btnPreOrden.UseVisualStyleBackColor = true;
+            this.btnPreOrden.Click += new System.EventHandler(this.btnPreOrden_Click);
+            // 
+            // lblRecorridos
+            // 
+            this.lblRecorridos.AutoSize = true;
+            this.lblRecorridos.Location = new System.Drawing.Point(12, 158);
+            this.lblRecorridos.Name = "lblRecorridos";
+            this.lblRecorridos.Size = new System.Drawing.Size(86, 20);
+            this.lblRecorridos.TabIndex = 7;
+            this.lblRecorridos.Text = "Recorridos";
+            // 
+            // btnPostOrden
+            // 
+            this.btnPostOrden.Enabled = false;
+            this.btnPostOrden.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnPostOrden.Location = new System.Drawing.Point(12, 265);
+            this.btnPostOrden.Name = "btnPostOrden";
+            this.btnPostOrden.Size = new System.Drawing.Size(161, 36);
+            this.btnPostOrden.TabIndex = 6;
+            this.btnPostOrden.Text = "Post-Orden";
+            this.btnPostOrden.UseVisualStyleBackColor = true;
+            this.btnPostOrden.Click += new System.EventHandler(this.btnPostOrden_Click);
+            // 
+            // btnInOrden
+            // 
+            this.btnInOrden.Enabled = false;
+            this.btnInOrden.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnInOrden.Location = new System.Drawing.Point(12, 181);
+            this.btnInOrden.Name = "btnInOrden";
+            this.btnInOrden.Size = new System.Drawing.Size(161, 36);
+            this.btnInOrden.TabIndex = 5;
+            this.btnInOrden.Text = "In-Orden";
+            this.btnInOrden.UseVisualStyleBackColor = true;
+            this.btnInOrden.Click += new System.EventHandler(this.btnInOrden_Click);
+            // 
+            // txtNumero
+            // 
+            this.txtNumero.Location = new System.Drawing.Point(12, 45);
+            this.txtNumero.Name = "txtNumero";
+            this.txtNumero.Size = new System.Drawing.Size(161, 26);
+            this.txtNumero.TabIndex = 4;
+            this.txtNumero.TextChanged += new System.EventHandler(this.txtNumero_TextChanged);
+            this.txtNumero.KeyPress += new System.Windows.Forms.KeyPressEventHandler(this.txtNumero_KeyPress);
+            // 
+            // lblValor
+            // 
+            this.lblValor.AutoSize = true;
+            this.lblValor.Location = new System.Drawing.Point(12, 22);
+            this.lblValor.Name = "lblValor";
+            this.lblValor.Size = new System.Drawing.Size(50, 20);
+            this.lblValor.TabIndex = 3;
+            this.lblValor.Text = "Valor:";
+            // 
+            // frmPrincipal
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(996, 527);
+            this.Controls.Add(this.groupBox1);
+            this.Name = "frmPrincipal";
+            this.Text = "Arbol Binario";
+            this.Paint += new System.Windows.Forms.PaintEventHandler(this.Form1_Paint);
+            this.MouseClick += new System.Windows.Forms.MouseEventHandler(this.frmPrincipal_MouseClick);
+            this.groupBox1.ResumeLayout(false);
+            this.groupBox1.PerformLayout();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button btnAddNode;
+        private System.Windows.Forms.Button btnDelNode;
+        private System.Windows.Forms.GroupBox groupBox1;
+        private System.Windows.Forms.TextBox txtNumero;
+        private System.Windows.Forms.Label lblValor;
+        private System.Windows.Forms.Button btnPreOrden;
+        private System.Windows.Forms.Label lblRecorridos;
+        private System.Windows.Forms.Button btnPostOrden;
+        private System.Windows.Forms.Button btnInOrden;
+        private System.Windows.Forms.Label lblSumatoria;
+        private System.Windows.Forms.RadioButton rbtnMultiplosCinco;
+        private System.Windows.Forms.RadioButton rbtnMultiplosTres;
+        private System.Windows.Forms.RadioButton rbtnMultiplosDos;
+        private System.Windows.Forms.RadioButton rbtnTotal;
+        private System.Windows.Forms.Label lblAltura;
+        private System.Windows.Forms.Label lblSumas;
+    }
+}
+
---
+++ Investigacion Complementaria - G6/Arbol Binario/Arbol Binario/Form1.cs
@@ -0,0 +1,317 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.Threading;
+
+namespace Arbol_Binario
+{
+    public partial class frmPrincipal : Form
+    {
+        public frmPrincipal()
+        {
+            InitializeComponent();
+        }
+
+        //Desmarcador
+        protected void desmarcarRadioButtons()
+        {
+            rbtnTotal.Checked = false;
+            rbtnMultiplosDos.Checked = false;
+            rbtnMultiplosTres.Checked = false;
+            rbtnMultiplosCinco.Checked = false;
+        }
+
+        //Deshabilitadores y Habilitadores
+        protected void habilitarButtons()
+        {
+            btnInOrden.Enabled = true;
+            btnPostOrden.Enabled = true;
+            btnPreOrden.Enabled = true;
+        }
+        protected void habilitarRadioButtons()
+        {
+            rbtnTotal.Enabled = true;
+            rbtnMultiplosDos.Enabled = true;
+            rbtnMultiplosTres.Enabled = true;
+            rbtnMultiplosCinco.Enabled = true;
+        }
+        protected void deshabilitarButtons()
+        {
+            btnInOrden.Enabled = false;
+            btnPostOrden.Enabled = false;
+            btnPreOrden.Enabled = false;
+        }
+        protected void deshabilitarRadioButtons()
+        {
+            rbtnTotal.Enabled = false;
+            rbtnMultiplosDos.Enabled = false;
+            rbtnMultiplosTres.Enabled = false;
+            rbtnMultiplosCinco.Enabled = false;
+        }
+
+
+        //Declaracion de variables a utilizar
+        int Dato = 0;
+        int cont = 0;
+        int suma = 0, suma2 = 0, suma3 = 0, suma5 = 0;
+
+        Arbol_Binario myTree = new Arbol_Binario(null); //Creacion del objeto Arbol
+        Graphics g;                                     //Definicion del objeto grafico
+
+        //Evento del formulario que permitira dibujar el Arbol Binario
+        private void Form1_Paint(object sender, PaintEventArgs e)
+        {
+            e.Graphics.Clear(this.BackColor);
+            e.Graphics.TextRenderingHint = System.Drawing.Text.TextRenderingHint.AntiAliasGridFit;
+            e.Graphics.SmoothingMode = System.Drawing.Drawing2D.SmoothingMode.AntiAlias;
+            g = e.Graphics;
+
+            myTree.DibujarArbol(g, this.Font, Brushes.Blue, Brushes.White, Pens.Black, Brushes.White);
+        }
+
+        private void btnAddNode_Click(object sender, EventArgs e)
+        {
+            Dato = int.Parse(txtNumero.Text);
+            myTree.Insertar(Dato);
+            txtNumero.Clear();
+            txtNumero.Focus();
+
+            cont++;
+            suma += Dato;
+            if(Dato%2 == 0)
+            {
+                suma2 += Dato;
+            }
+            if(Dato%3 == 0)
+            {
+                suma3 += Dato;
+            }
+            if(Dato%5 == 0)
+            {
+                suma5 += Dato;
+            }
+
+            lblAltura.Text = "Altura: " + myTree.calcularAltura(myTree.Raiz);
+            lblAltura.Visible = true;
+            habilitarButtons();
+            habilitarRadioButtons();
+            desmarcarRadioButtons();
+            Refresh();
+        }
+
+        private void btnDelNode_Click(object sender, EventArgs e)
+        {
+            Dato = int.Parse(txtNumero.Text);
+            myTree.Eliminar(Dato);
+            txtNumero.Clear();
+            txtNumero.Focus();
+
+            cont--;
+            suma -= Dato;
+            if (Dato%2 == 0)
+            {
+                suma2 -= Dato;
+            }
+            if (Dato%3 == 0)
+            {
+                suma3 -= Dato;
+            }
+            if (Dato%5 == 0)
+            {
+                suma5 -= Dato;
+            }
+
+            if (cont == 0)
+            {
+                deshabilitarButtons();
+                deshabilitarRadioButtons();
+                lblAltura.Visible = false;
+            }
+            desmarcarRadioButtons();
+            Refresh();
+        }
+
+        private void txtNumero_TextChanged(object sender, EventArgs e)
+        {
+            Refresh();
+            if (txtNumero.Text != String.Empty && txtNumero.Text != "-")
+            {
+                try
+                {
+                    int num;
+                    num = Convert.ToInt32(txtNumero.Text);
+                    if (num > 99 || num < -99)
+                    {
+                        txtNumero.BackColor = Color.Orange;
+                        btnAddNode.Enabled = false;
+                        btnDelNode.Enabled = false;
+                        MessageBox.Show("No se admiten numeros tan grandes", "Advertencia", MessageBoxButtons.OK, MessageBoxIcon.Exclamation);
+                    }
+                    else
+                    {
+                        if (myTree.existe(num, myTree.Raiz))
+                        {
+                            g = this.CreateGraphics();
+                            btnAddNode.Enabled = false;
+                            btnDelNode.Enabled = true;
+                            myTree.resaltar(g, this.Font, Brushes.Blue, Brushes.White, Pens.Black, myTree.Raiz, num);
+                            txtNumero.BackColor = Color.Red;
+                        }
+                        else
+                        {
+                            btnAddNode.Enabled = true;
+                            btnDelNode.Enabled = false;
+                            txtNumero.BackColor = Color.Green;
+                        }
+                    }
+                }
+                catch
+                {
+                    MessageBox.Show("No se admiten numeros tan grandes 2", "Advertencia", MessageBoxButtons.OK, MessageBoxIcon.Exclamation);
+                    txtNumero.BackColor = Color.Orange;
+                }
+            }
+            else
+            {
+                txtNumero.BackColor = Color.Red;
+                btnAddNode.Enabled = false;
+                btnDelNode.Enabled = false;
+            }
+        }
+
+        private void txtNumero_KeyPress(object sender, KeyPressEventArgs e)
+        {
+            if (!(char.IsNumber(e.KeyChar)) && (e.KeyChar != (char)Keys.Back) && (e.KeyChar != '-'))
+            {
+                MessageBox.Show("Solo se permiten numeros", "Advertencia", MessageBoxButtons.OK, MessageBoxIcon.Exclamation);
+                e.Handled = true;
+                return;
+            }
+            desmarcarRadioButtons();
+        }
+
+        private void btnInOrden_Click(object sender, EventArgs e)
+        {
+            this.Cursor = Cursors.WaitCursor;
+            Refresh();
+            desmarcarRadioButtons();
+            txtNumero.Clear();
+            deshabilitarButtons();
+            deshabilitarRadioButtons();
+            g = this.CreateGraphics();
+            myTree.colorear(g, this.Font, Brushes.Blue, Brushes.White, Pens.Black, myTree.Raiz, false, true, false);
+            habilitarButtons();
+            habilitarRadioButtons();
+            this.Cursor = Cursors.Default;
+        }
+
+        private void btnPreOrden_Click(object sender, EventArgs e)
+        {
+            this.Cursor = Cursors.WaitCursor;
+            Refresh();
+            desmarcarRadioButtons();
+            txtNumero.Clear();
+            deshabilitarButtons();
+            deshabilitarRadioButtons();
+            g = this.CreateGraphics();
+            myTree.colorear(g, this.Font, Brushes.Blue, Brushes.White, Pens.Black, myTree.Raiz, false, false, true);
+            habilitarButtons();
+            habilitarRadioButtons();
+            this.Cursor = Cursors.Default;
+        }
+
+        private void btnPostOrden_Click(object sender, EventArgs e)
+        {
+            this.Cursor = Cursors.WaitCursor;
+            Refresh();
+            desmarcarRadioButtons();
+            txtNumero.Clear();
+            deshabilitarButtons();
+            deshabilitarRadioButtons();
+            g = this.CreateGraphics();
+            myTree.colorear(g, this.Font, Brushes.Blue, Brushes.White, Pens.Black, myTree.Raiz, true, false, false);
+            habilitarButtons();
+            habilitarRadioButtons();
+            this.Cursor = Cursors.Default;
+        }
+
+        private void frmPrincipal_MouseClick(object sender, MouseEventArgs e)
+        {
+            int valor = 100;
+            Refresh();
+            txtNumero.Clear();
+            g = this.CreateGraphics();
+            valor = myTree.seleccionar(g, this.Font, Brushes.White, Pens.Black, myTree.Raiz, e.Location);
+            if (valor != 100)
+            {
+                txtNumero.Text = valor.ToString();
+            }
+        }
+
+        private void rbtnTotal_CheckedChanged(object sender, EventArgs e)
+        {
+            if (rbtnTotal.Checked)
+            {
+                Refresh();
+                txtNumero.Clear();
+                g = this.CreateGraphics();
+                myTree.sumar(g, this.Font, Brushes.Blue, Brushes.White, Pens.Black, myTree.Raiz, 1);
+                lblSumatoria.Visible = true;
+                lblSumatoria.Text = "Suma: " + suma.ToString();
+            }
+            else
+                lblSumatoria.Visible = false;
+        }
+
+        private void rbtnMultiplosDos_CheckedChanged(object sender, EventArgs e)
+        {
+            if (rbtnMultiplosDos.Checked)
+            {
+                Refresh();
+                txtNumero.Clear();
+                g = this.CreateGraphics();
+                myTree.sumar(g, this.Font, Brushes.Blue, Brushes.White, Pens.Black, myTree.Raiz, 2);
+                lblSumatoria.Visible = true;
+                lblSumatoria.Text = "Suma: " + suma2.ToString();
+            }
+            else
+                lblSumatoria.Visible = false;
+        }
+
+        private void rbtnMultiplosTres_CheckedChanged(object sender, EventArgs e)
+        {
+            if (rbtnMultiplosTres.Checked)
+            {
+                Refresh();
+                txtNumero.Clear();
+                g = this.CreateGraphics();
+                myTree.sumar(g, this.Font, Brushes.Blue, Brushes.White, Pens.Black, myTree.Raiz, 3);
+                lblSumatoria.Visible = true;
+                lblSumatoria.Text = "Suma: " + suma3.ToString();
+            }
+            else
+                lblSumatoria.Visible = false;
+        }
+
+        private void rbtnMultiplosCinco_CheckedChanged(object sender, EventArgs e)
+        {
+            if (rbtnMultiplosCinco.Checked)
+            {
+                Refresh();
+                txtNumero.Clear();
+                g = this.CreateGraphics();
+                myTree.sumar(g, this.Font, Brushes.Blue, Brushes.White, Pens.Black, myTree.Raiz, 5);
+                lblSumatoria.Visible = true;
+                lblSumatoria.Text = "Suma: " + suma5.ToString();
+            }
+            else
+                lblSumatoria.Visible = false;
+        }
+    }
+}
