8dacf117142612a8607341215d5c18ae11b6349d
Cristian Sanchez <sancas19@gmail.com>
2016-08-17 19:08:37 -0600

    Terminado el funcionamiento agregando errores con error provider

--- Tarea 1/Problema1/Problema1/Form1.Designer.cs
+++ Tarea 1/Problema1/Problema1/Form1.Designer.cs
@@ -28,20 +28,158 @@
         /// </summary>
         private void InitializeComponent()
         {
+            this.components = new System.ComponentModel.Container();
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form1));
+            this.metroLabel1 = new MetroFramework.Controls.MetroLabel();
+            this.txtPrestamo = new MetroFramework.Controls.MetroTextBox();
+            this.errores = new System.Windows.Forms.ErrorProvider(this.components);
+            this.metroLabel2 = new MetroFramework.Controls.MetroLabel();
+            this.txtTasa = new MetroFramework.Controls.MetroTextBox();
+            this.metroLabel3 = new MetroFramework.Controls.MetroLabel();
+            this.txtMeses = new MetroFramework.Controls.MetroTextBox();
+            this.txtCuota = new MetroFramework.Controls.MetroTextBox();
+            this.btnCalcular = new MetroFramework.Controls.MetroButton();
+            ((System.ComponentModel.ISupportInitialize)(this.errores)).BeginInit();
             this.SuspendLayout();
+            // 
+            // metroLabel1
+            // 
+            this.metroLabel1.AutoSize = true;
+            this.metroLabel1.FontSize = MetroFramework.MetroLabelSize.Tall;
+            this.metroLabel1.ForeColor = System.Drawing.Color.Black;
+            this.metroLabel1.Location = new System.Drawing.Point(150, 100);
+            this.metroLabel1.Name = "metroLabel1";
+            this.metroLabel1.Size = new System.Drawing.Size(161, 25);
+            this.metroLabel1.Style = MetroFramework.MetroColorStyle.Red;
+            this.metroLabel1.TabIndex = 0;
+            this.metroLabel1.Text = "Prestamo adquirido";
+            this.metroLabel1.UseStyleColors = true;
+            // 
+            // txtPrestamo
+            // 
+            this.txtPrestamo.FontSize = MetroFramework.MetroTextBoxSize.Medium;
+            this.txtPrestamo.Location = new System.Drawing.Point(350, 100);
+            this.txtPrestamo.Name = "txtPrestamo";
+            this.txtPrestamo.PromptText = "$";
+            this.txtPrestamo.Size = new System.Drawing.Size(150, 25);
+            this.txtPrestamo.Style = MetroFramework.MetroColorStyle.Red;
+            this.txtPrestamo.TabIndex = 1;
+            this.txtPrestamo.Text = " ";
+            this.txtPrestamo.UseStyleColors = true;
+            // 
+            // errores
+            // 
+            this.errores.ContainerControl = this;
+            this.errores.Icon = ((System.Drawing.Icon)(resources.GetObject("errores.Icon")));
+            // 
+            // metroLabel2
+            // 
+            this.metroLabel2.AutoSize = true;
+            this.metroLabel2.FontSize = MetroFramework.MetroLabelSize.Tall;
+            this.metroLabel2.Location = new System.Drawing.Point(150, 150);
+            this.metroLabel2.Name = "metroLabel2";
+            this.metroLabel2.Size = new System.Drawing.Size(122, 25);
+            this.metroLabel2.Style = MetroFramework.MetroColorStyle.Red;
+            this.metroLabel2.TabIndex = 2;
+            this.metroLabel2.Text = "Tasa de interes";
+            this.metroLabel2.UseStyleColors = true;
+            // 
+            // txtTasa
+            // 
+            this.txtTasa.FontSize = MetroFramework.MetroTextBoxSize.Medium;
+            this.txtTasa.Location = new System.Drawing.Point(350, 150);
+            this.txtTasa.Name = "txtTasa";
+            this.txtTasa.PromptText = "%";
+            this.txtTasa.Size = new System.Drawing.Size(150, 25);
+            this.txtTasa.Style = MetroFramework.MetroColorStyle.Red;
+            this.txtTasa.TabIndex = 3;
+            this.txtTasa.Text = " ";
+            this.txtTasa.UseStyleColors = true;
+            // 
+            // metroLabel3
+            // 
+            this.metroLabel3.AutoSize = true;
+            this.metroLabel3.FontSize = MetroFramework.MetroLabelSize.Tall;
+            this.metroLabel3.Location = new System.Drawing.Point(150, 200);
+            this.metroLabel3.Name = "metroLabel3";
+            this.metroLabel3.Size = new System.Drawing.Size(151, 25);
+            this.metroLabel3.Style = MetroFramework.MetroColorStyle.Red;
+            this.metroLabel3.TabIndex = 4;
+            this.metroLabel3.Text = "Numero de meses";
+            this.metroLabel3.UseStyleColors = true;
+            // 
+            // txtMeses
+            // 
+            this.txtMeses.FontSize = MetroFramework.MetroTextBoxSize.Medium;
+            this.txtMeses.Location = new System.Drawing.Point(350, 200);
+            this.txtMeses.Name = "txtMeses";
+            this.txtMeses.PromptText = "#";
+            this.txtMeses.Size = new System.Drawing.Size(150, 25);
+            this.txtMeses.Style = MetroFramework.MetroColorStyle.Red;
+            this.txtMeses.TabIndex = 5;
+            this.txtMeses.Text = " ";
+            this.txtMeses.UseStyleColors = true;
+            // 
+            // txtCuota
+            // 
+            this.txtCuota.FontSize = MetroFramework.MetroTextBoxSize.Tall;
+            this.txtCuota.Location = new System.Drawing.Point(257, 291);
+            this.txtCuota.Name = "txtCuota";
+            this.txtCuota.PromptText = "Cuota mensual";
+            this.txtCuota.ReadOnly = true;
+            this.txtCuota.Size = new System.Drawing.Size(135, 30);
+            this.txtCuota.Style = MetroFramework.MetroColorStyle.Red;
+            this.txtCuota.TabIndex = 7;
+            this.txtCuota.Text = " ";
+            this.txtCuota.UseStyleColors = true;
+            // 
+            // btnCalcular
+            // 
+            this.btnCalcular.Highlight = true;
+            this.btnCalcular.Location = new System.Drawing.Point(287, 250);
+            this.btnCalcular.Name = "btnCalcular";
+            this.btnCalcular.Size = new System.Drawing.Size(75, 25);
+            this.btnCalcular.Style = MetroFramework.MetroColorStyle.Red;
+            this.btnCalcular.TabIndex = 6;
+            this.btnCalcular.Text = "Calcular";
+            this.btnCalcular.Click += new System.EventHandler(this.btnCalcular_Click);
             // 
             // Form1
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(696, 438);
+            this.ClientSize = new System.Drawing.Size(696, 387);
+            this.Controls.Add(this.btnCalcular);
+            this.Controls.Add(this.txtCuota);
+            this.Controls.Add(this.txtMeses);
+            this.Controls.Add(this.metroLabel3);
+            this.Controls.Add(this.txtTasa);
+            this.Controls.Add(this.metroLabel2);
+            this.Controls.Add(this.txtPrestamo);
+            this.Controls.Add(this.metroLabel1);
+            this.MaximizeBox = false;
             this.Name = "Form1";
+            this.Resizable = false;
+            this.Style = MetroFramework.MetroColorStyle.Red;
             this.Text = "Calculadora cuota mensual";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            ((System.ComponentModel.ISupportInitialize)(this.errores)).EndInit();
             this.ResumeLayout(false);
+            this.PerformLayout();
 
         }
 
         #endregion
+
+        private MetroFramework.Controls.MetroLabel metroLabel1;
+        private MetroFramework.Controls.MetroTextBox txtPrestamo;
+        private System.Windows.Forms.ErrorProvider errores;
+        private MetroFramework.Controls.MetroTextBox txtTasa;
+        private MetroFramework.Controls.MetroLabel metroLabel2;
+        private MetroFramework.Controls.MetroLabel metroLabel3;
+        private MetroFramework.Controls.MetroTextBox txtMeses;
+        private MetroFramework.Controls.MetroTextBox txtCuota;
+        private MetroFramework.Controls.MetroButton btnCalcular;
     }
 }
 
--- Tarea 1/Problema1/Problema1/Form1.cs
+++ Tarea 1/Problema1/Problema1/Form1.cs
@@ -6,7 +6,6 @@ using System.Drawing;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
-//using System.Windows.Forms;
 using MetroFramework.Forms;
 
 namespace Problema1
@@ -16,6 +15,41 @@ namespace Problema1
         public Form1()
         {
             InitializeComponent();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            txtPrestamo.Text = String.Empty;
+            txtTasa.Text = String.Empty;
+            txtMeses.Text = String.Empty;
+            txtCuota.Text = String.Empty;
+            txtPrestamo.Focus();
+        }
+
+        private void btnCalcular_Click(object sender, EventArgs e)
+        {
+            try
+            {
+                double c, p, i;
+                int n;
+                p = Convert.ToDouble(txtPrestamo.Text);
+                i = Convert.ToDouble(txtTasa.Text);
+                n = Convert.ToInt32(txtMeses.Text);
+                c = p * ((i/100 * Math.Pow(1 + i/100, n)) / ((Math.Pow(1 + i/100, n) - 1)));
+                txtCuota.Text = c.ToString();
+            }
+            catch (Exception)
+            {
+                var texts = Controls.OfType<MetroFramework.Controls.MetroTextBox>();
+                foreach (var text in texts)
+                {
+                    if (string.IsNullOrWhiteSpace(text.Text) && text.Name != "txtCuota")
+                    {
+                        errores.SetError(text, "Por favor llena los campos requeridos");
+                    }
+                    if (text.Text.Contains())
+                }
+            }
         }
     }
 }
