e98504cd207c1d260065de14e7a8da4ebb5f6971
Cristian Sanchez <sancas19@gmail.com>
2016-08-18 02:44:10 -0600

    Se termino la tarea 1
    Se termin la tarea 1

---
+++ Tarea 1/Problema3/Problema3/myExtension.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ExtensionMethods
+{
+    public static class myExtension
+    {
+        public static bool IsLetters(this string sCaracteres)
+        {
+            foreach (char ch in sCaracteres)
+            {
+                if (!Char.IsLetter(ch) && ch != 32)
+                {
+                    return false;
+                }
+            }
+            return true;
+        }
+
+        public static bool IsNumber(this string sCaracteres)
+        {
+            foreach (char ch in sCaracteres)
+            {
+                if (!Char.IsNumber(ch))
+                {
+                    return false;
+                }
+            }
+            return true;
+        }
+    }
+}
